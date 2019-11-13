886e5ac9acae28268cfac4ff56954f8edcac2675
JavaProg <msh86.02.18@rambler.ru>
2019-11-13 22:59:17 +0500

    Домашнее задание №4 Добавил управление KeyDown, KeyUp, touchDown, touchDragged Разделил текстуру корабля.

--- core/src/shein/firstgame/screen/GameScreen.java
+++ core/src/shein/firstgame/screen/GameScreen.java
@@ -11,6 +11,7 @@ import shein.firstgame.base.BaseScreen;
 import shein.firstgame.math.Rect;
 import shein.firstgame.sprite.Backgroung;
 import shein.firstgame.sprite.Star;
+import shein.firstgame.sprite.StarShip;
 
 
 public class GameScreen extends BaseScreen {
@@ -19,18 +20,24 @@ public class GameScreen extends BaseScreen {
     private TextureAtlas atlas;
     private Backgroung bg;
     private Star[] stars;
-    private int STAR_COUNT = 50;
+    private int STAR_COUNT = 25;
+    private StarShip starship;
+    private TextureRegion main_ship;
+    private Texture ship;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("textures/bg.png");
+        ship = new Texture("textures/mainAtlas.png");
         bg = new Backgroung(new TextureRegion(img));
         atlas = new TextureAtlas("textures/mainAtlas.tpack");
         stars = new Star[STAR_COUNT];
         for (int i = 0; i < STAR_COUNT; i++) {
             stars[i] = new Star(atlas);
         }
+        main_ship = new TextureRegion(ship,916, 95, 195, 287);
+        starship = new StarShip(main_ship);
     }
 
     @Override
@@ -46,6 +53,7 @@ public class GameScreen extends BaseScreen {
         for (Star s: stars) {
             s.resize(worldBounds);
         }
+        starship.resize(worldBounds);
     }
 
     @Override
@@ -57,29 +65,39 @@ public class GameScreen extends BaseScreen {
 
     @Override
     public boolean keyDown(int keycode) {
-        return super.keyDown(keycode);
+        starship.keyDown(keycode);
+        return false;
     }
 
     @Override
     public boolean keyUp(int keycode) {
-        return super.keyUp(keycode);
+        starship.keyUp(keycode);
+        return false;
     }
 
     @Override
     public boolean touchDown(Vector2 target, int pointer) {
-        return super.touchDown(target, pointer);
+        starship.touchDown(target, pointer);
+        return false;
     }
 
     @Override
     public boolean touchUp(Vector2 target, int pointer) {
-        return super.touchUp(target, pointer);
+        starship.touchUp(target, pointer);
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(Vector2 target, int pointer) {
+        starship.touchDragged(target, pointer);
+        return false;
     }
 
     private void update (float delta){
         for (Star s: stars) {
             s.update(delta);
         }
-
+        starship.update(delta);
     }
 
     private void draw(){
@@ -90,10 +108,7 @@ public class GameScreen extends BaseScreen {
         for (Star s: stars) {
             s.draw(batch);
         }
-
+        starship.draw(batch);
         batch.end();
     }
-
-
-
 }
